2d76c21dd1483106b2ef253b82f867e7f4e6d525
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-02-27 03:57:54 -0300

    Adicionando transição a Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Historico from "../../assets/Header/bHistorico.png";
 import Home from "../../assets/Dashboard/bnHome.png";
@@ -9,43 +10,62 @@ import Fechar from "../../assets/Header/bFecharHeader.png";
 import styles from './Navbar.module.scss';
 
 function Navbar() {
+    //useNavigate se refere as rotas de navegação nas páginas.
   const navigate = useNavigate();
-    return (
-        <>
-            <nav className={styles.navBarEstilo}>
-                    <button className={styles.FecharEAbrir} onClick={ ()=>{navigate("/")} }>
-                        <img src={Fechar} alt="imagem do Botão de abrir e fechar menun de navegação" />
-                    </button>
-                   
-                <section>
-                   
-                    <img src={Perfil} alt="imagem do botão de Perfil" />
-        
-                    <button onClick={ ()=>{navigate("/reembolsos")}}>
-                        <img src={Home} alt="imagem do botão de Pagina inicial" />
-                    </button>
-        
-                    <button onClick={ ()=> {navigate('/solicitacao')}}>
-                        <img src={Reembolso} alt="imagem do botão de Solicitação de Reembolso" />
-                    </button>
-        
-                    <button>
-                        <img src={Pesquisa} alt="imagem do botão de pesquisar" />
-                    </button>
-        
-                    <button>
-                        <img src={Historico} alt="imagem do botão de historico" />
-                    </button>
-        
-                </section>
+  //useState para trocar de um estado para outro. Neste caso usado para alterar estilo da navbar.
+  const [isOpen, setIsOpen] = useState(false); // Estado para abrir/fechar a sidebar
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen); // Alterna entre aberto e fechado
+  };
 
-                    <button className={styles.BotaoSair} onClick={ ()=>{navigate("/")}}>
-                        <img src={Sair} alt="imagem do botão de sair" />
-                    </button>
+  return (
+    <>
+      <nav className={`${styles.navBarEstilo} ${isOpen ? styles.aberto : styles.fechado}`}>
+        {/* Foi trocado a classe fixa "styles.navBarEstilo" por uma template string onde adicionei classes condicionais. Representado pela estrutura (condição ? valor_se_verdadeiro : valor_se_falso;) */}
+        <button className={styles.FecharEAbrir} onClick={toggleSidebar}>
+          <img className={styles.imgPerfil} src={Fechar} alt="Botão de abrir e fechar menu de navegação" />
+        </button>
 
-            </nav>
-        
-        </>
-    );
+        <section>
+          <img src={Perfil} alt="Botão de Perfil" />
+
+          <div className={styles.DivBotaoMenuAberto}>
+              <button onClick={() => navigate("/reembolsos")}>
+                <img src={Home} alt="Botão de Página Inicial" />
+              </button>
+                <p>Início</p>
+          </div>
+          
+
+          <div className={styles.DivBotaoMenuAberto}>
+              <button onClick={() => navigate('/solicitacao')}>
+                <img src={Reembolso} alt="Botão de Solicitação de Reembolso" />
+              </button>
+              <p>Reembolsos</p>
+          </div>
+
+          <div className={styles.DivBotaoMenuAberto}>
+              <button>
+                <img src={Pesquisa} alt="Botão de pesquisar" />
+              </button>
+              <p>Análises</p>
+          </div>
+
+          <div className={styles.DivBotaoMenuAberto}>
+              <button>
+                <img src={Historico} alt="Botão de histórico" />
+              </button>
+                <p>Histórico</p>
+          </div>
+
+        </section>
+
+        <button className={styles.BotaoSair} onClick={() => navigate("/")}>
+          <img src={Sair} alt="Botão de sair" />
+        </button>
+      </nav>
+    </>
+  );
 }
+
 export default Navbar;
