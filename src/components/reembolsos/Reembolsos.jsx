337dc7aeedf0178b61158f3750508851632689a0
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-04-20 12:44:03 -0300

    ajustando estilos do componente Navbar e reorganizando o layout da página de reembolsos

--- src/components/reembolsos/Reembolsos.jsx
+++ src/components/reembolsos/Reembolsos.jsx
@@ -17,8 +17,8 @@ function Reembolsos() {
 
   const navigate = useNavigate();
   const IrParaSolicitacao = () => {
-    navigate('/solicitacao');  
-    
+    navigate('/solicitacao');
+
   }
 
   return (
@@ -34,72 +34,66 @@ function Reembolsos() {
             </div>
           </header>
         </div>
-        
-            <main className={styles.mainReembolsos}>
-        <section className={styles.centralizar}>
-          <div className={styles.textoReembolso}>
-            <h1>Sistema de Reembolso</h1>
-            <p>Solicite novos pedidos de reembolso, visualize solicitações em análise e todo o histórico.</p>
-          </div>
-          <section className={styles.containerCards}>
-            <article className={styles.CardReembolso} onClick={IrParaSolicitacao}>
-              <img src={SolicitarReembolso} alt="icone para solicitação de reembolso" />
-              <h3> Solicitar Reembolso </h3>
-            </article>
-        
-            <article className={styles.CardAnalise}>
-              <img src={Analises} alt="icone para analises" />
-              <h3> Verificar análises </h3>
-            </article>
-        
-            <article className={styles.CardHistorico}>
-              <img src={SolicitarHistorico} alt="Icone de historico" />
-              <h3> Histórico </h3>
-            </article>
-          </section>
-          
-          <section className={styles.containerStatus}>
-            <div>
-              <img className={styles.img1} src={NumeroSolicitados} alt="" />
-              <h4>1</h4>
-              <p>Solicitados</p>
-            </div>
-        
-            <div>
-              <img className={styles.img2} src={NumeroAnalises} alt="" />
-              <h4>2</h4>
-              <p>Em análise</p>
-            </div>
-        
-            <div>
-              <img className={styles.img3} src={NumeroAprovados} alt="" />
-              <h4>3</h4>
-              <p>Aprovados</p>
-            </div>
-        
-            <div>
-              <img className={styles.img4}src={NumeroRejeitados} alt="" />
-              <h4>4</h4>
-              <p>Rejeitados</p>
-            </div>
-          </section>
-          <section className={styles.containerSistema} >
-            <img src={Sistema} alt="Icone de sistema atualizado" />
-            <p>Sistema atualizado</p>
-        
-          </section>
-        </section>
-        
-        
-            </main>
 
-            
+        <main className={styles.mainReembolsos}>
+          <section className={styles.centralizar}>
+            <div className={styles.textoReembolso}>
+              <h1>Sistema de Reembolso</h1>
+              <p>Solicite novos pedidos de reembolso, visualize solicitações em análise e todo o histórico.</p>
+            </div>
+            <section className={styles.containerCards}>
+              <article className={styles.CardReembolso} onClick={IrParaSolicitacao}>
+                <img src={SolicitarReembolso} alt="icone para solicitação de reembolso" />
+                <h3> Solicitar Reembolso </h3>
+              </article>
+
+              <article className={styles.CardAnalise}>
+                <img src={Analises} alt="icone para analises" />
+                <h3> Verificar análises </h3>
+              </article>
+
+              <article className={styles.CardHistorico}>
+                <img src={SolicitarHistorico} alt="Icone de historico" />
+                <h3> Histórico </h3>
+              </article>
+            </section>
+
+            <section className={styles.containerStatus}>
+              <div>
+                <img className={styles.img1} src={NumeroSolicitados} alt="" />
+                <h4>1</h4>
+                <p>Solicitados</p>
+              </div>
+
+              <div>
+                <img className={styles.img2} src={NumeroAnalises} alt="" />
+                <h4>2</h4>
+                <p>Em análise</p>
+              </div>
+
+              <div>
+                <img className={styles.img3} src={NumeroAprovados} alt="" />
+                <h4>3</h4>
+                <p>Aprovados</p>
+              </div>
+
+              <div>
+                <img className={styles.img4} src={NumeroRejeitados} alt="" />
+                <h4>4</h4>
+                <p>Rejeitados</p>
+              </div>
+            </section>
+            <section className={styles.containerSistema} >
+              <img src={Sistema} alt="Icone de sistema atualizado" />
+              <p>Sistema atualizado</p>
+
+            </section>
+          </section>
+
+
+        </main>
 
       </div>
-
-
-
-
     </div>
   );
 }
