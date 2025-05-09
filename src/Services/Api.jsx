01521570c947da86eca259e6a34fa1dc5e18118f
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-05-09 13:28:13 -0300

    atualizando a configuração da URL base da API para desenvolvimento local

--- src/Services/Api.jsx
+++ src/Services/Api.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 //cria uma instância do axios com a URL base da API
 const api = axios.create({
-    baseURL: "https://api-t1-8fja.onrender.com", //Define a url base para todas as requisições feitas com essa instancia. Essa url é a url do servidor que está rodando a API.
+    // baseURL:"http://localhost:5000" //URL para desolvovimento local
+    baseURL: "https://api-t1-8fja.onrender.com" //Define a url base para todas as requisições feitas com essa instancia. Essa url é a url do servidor que está rodando a API.
     })
 export default api;
