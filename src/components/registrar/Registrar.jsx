2f87053c79f94c91403bcc4a1572276e93c0c1d3
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-05-12 00:57:44 -0300

    implementando a página de registro de colaboradores com formulário e estilos

---
+++ src/components/registrar/Registrar.jsx
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import axios from 'axios';
+import styles from './Registrar.module.scss';
+import Logo from "../../assets/Tela Login/logo-ws.png";
+import { useNavigate } from 'react-router-dom';
+
+
+export default function Register() {
+  const navigate = useNavigate();
+  
+  const [formData, setFormData] = useState({
+    nome: '',
+    email: '',
+    senha: '',
+    cargo: '',
+    salario: '',
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFormData(prev => ({ ...prev, [name]: value }));
+  };
+
+  const IrParaLogin = () => {
+  navigate('/');
+}
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      await axios.post('https://api-t1-8fja.onrender.com', formData); // URL da API
+      alert('Colaborador registrado com sucesso!');
+    } catch (error) {
+      console.error('Erro ao registrar colaborador:', error);
+      alert('Erro no registro. Verifique os dados.');
+    }
+  };
+
+  return (
+    <div className={styles.registerPage}>
+      <form onSubmit={handleSubmit} className={styles.registerForm}>
+        <div className={styles.header}>
+          <img src={Logo} alt="Logo da Wilson Sons" className={styles.logo} />
+          <div className={styles.textos}>
+            <h2>Cadastro de colaboradores</h2>
+            <p>Obtenha acesso ao Portal SISPAR</p>
+          </div>
+        </div>
+        
+
+        <label>
+          Nome
+          <input type="text" name="nome" value={formData.nome} onChange={handleChange} required />
+        </label>
+
+        <label>
+          Email
+          <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+        </label>
+
+        <label>
+          Senha
+          <input type="password" name="senha" value={formData.senha} onChange={handleChange} required />
+        </label>
+
+        <label>
+          Cargo
+          <input type="text" name="cargo" value={formData.cargo} onChange={handleChange} required />
+        </label>
+
+        <label>
+          Salário
+          <input type="number" name="salario" value={formData.salario} onChange={handleChange} required />
+        </label>
+
+        <button type="submit">Registrar</button>
+
+        <button
+          type="button"
+          className={styles.btnVoltar}
+          onClick={IrParaLogin}
+        >
+          Voltar
+        </button>
+      </form>
+    </div>
+  );
+}
