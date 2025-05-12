1843cbfa318bee6f5c32b5a5011b2cc44c6ae7d2
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-05-12 12:37:04 -0300

    refatorando o componente de login para melhorar a legibilidade e a estrutura do código

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -7,45 +7,37 @@ import { useState } from "react";
 
 function Login() {
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const IrParaReembolso = () => {
-  navigate('/reembolsos');
-}
-
-const IrParaCadastrar = () => {
-navigate('/cadastrar');
-}
-
-//iniciando os estados para email e senha
-const [email, setEmail] = useState("");
-const [senha, setSenha] = useState("");
-
-const fazerLogin = async (e) => {
-  e.preventDefault();
-  try {
-    // 1. Login
-    const resposta = await Api.post("/colaborador/login", { 
-      email, 
-      senha 
-    });//fazendo a requisição POST para a API com o email e senha do usuário
-
-    alert("Login realizado com sucesso!");
-
-    // 2. Buscar ID do colaborador no banco de dados
-    const respostaId = await Api.post("/colaborador/buscar_id", { email });
-    const idColaborador = respostaId.data.id_colaborador;
-
-    // 3. Armazenar localmente o id do colaborador
-    localStorage.setItem("idColaborador", idColaborador);
-
-    // 4. Redirecionar
-    IrParaReembolso();
-  } catch (error) {
-    console.error("Erro ao fazer login:", error);
-    alert("Erro ao fazer login. Verifique suas credenciais e tente novamente.");
+  const IrParaReembolso = () => {
+    navigate('/reembolsos');
   }
-}
+
+  const IrParaCadastrar = () => {
+    navigate('/cadastrar');
+  }
+
+  //iniciando os estados para email e senha
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const fazerLogin = async (e) => {
+    e.preventDefault(); //previne o comportamento padrão do formulário de recarregar a página
+    try {
+      const resposta = await Api.post("/colaborador/login", {
+        "email": email,
+        "senha": senha
+      }) //fazendo a requisição POST para a API com o email e senha do usuário
+
+      console.log(resposta.data); //exibe a resposta da API no console
+      alert("Login realizado com sucesso!"); //exibe um alerta de sucesso
+      IrParaReembolso(); //chama a função para redirecionar o usuário para a página de reembolso
+
+    } catch (error) {
+      console.error("Erro ao fazer login:", error)
+      alert("Erro ao fazer login. Verifique suas credenciais e tente novamente.")
+    }
+  }
 
 
   return (
@@ -64,43 +56,43 @@ const fazerLogin = async (e) => {
 
         <form action="">
           <div>
-            <input type="email" 
-            name="email" 
-            id="email" 
-            placeholder=" Email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)}/>
+            <input type="email"
+              name="email"
+              id="email"
+              placeholder=" Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} />
           </div>
-          
+
           <div>
-            <input type="password" 
-            name="senha" 
-            id="senha" 
-            placeholder=" Senha"
-            value = {senha}
-            onChange={(e) => setSenha(e.target.value)}
+            <input type="password"
+              name="senha"
+              id="senha"
+              placeholder=" Senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
             />
           </div>
-          
+
           <div>
             <a href="#" className={styles.EsqueciSenha}>Esqueci minha senha</a>
           </div>
-          
+
           <div className={styles.divBotoes}>
-          
-          <button 
-          className={styles.botaoEntrar} 
-          onClick={fazerLogin}
-          > 
-            Entrar
-          </button>
-          
-          <button 
-          className={styles.botaoCriarConta} 
-          onClick={IrParaCadastrar}
-          > 
-            Criar conta
-          </button>
+
+            <button
+              className={styles.botaoEntrar}
+              onClick={fazerLogin}
+            >
+              Entrar
+            </button>
+
+            <button
+              className={styles.botaoCriarConta}
+              onClick={IrParaCadastrar}
+            >
+              Criar conta
+            </button>
 
           </div>
         </form>
