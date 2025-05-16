057b40a131592f5e2a60e39ad708284214c020c5
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-05-16 11:12:41 -0300

    alterando a URL base no arquivo Api.jsx de localhost para PROD

--- src/Services/Api.jsx
+++ src/Services/Api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 //cria uma instância do axios com a URL base da API
 const api = axios.create({
-    baseURL:"http://localhost:5000" //URL para desenvolvimento local
-    // baseURL: "https://api-t1-rxlj.onrender.com" //Define a url base para todas as requisições feitas com essa instancia. Essa url é a url do servidor que está rodando a API.
+    // baseURL:"http://localhost:5000" //URL para desenvolvimento local
+    baseURL: "https://api-t1-rxlj.onrender.com" //Define a url base para todas as requisições feitas com essa instancia. Essa url é a url do servidor que está rodando a API.
     })
 export default api;
